9dd8f76f6f4ebbd83898d52a3e7f33be0047f720
George Njoroge <georgenjoroge977@gmail.com>
2019-02-08 02:16:33 +0300

    solving the mysql migration error, models relationships

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
